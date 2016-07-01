5779cead6b5c56027c2f1d0be49d724ab24caefe
khuang93 <kailin-huang@outlook.com>
2016-07-02 01:33:25 +0200

    init

---
+++ src/amzposttool/AMZPostTool.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amzposttool;
+
+/**
+ *
+ * @author Kailin Huang
+ */
+public class AMZPostTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public AMZPostTool() {
+        
+    }
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ToolBasicFrame basicFrame= new ToolBasicFrame();
+        basicFrame.setVisible(true);
+    
+        
+    }
+    
+}
